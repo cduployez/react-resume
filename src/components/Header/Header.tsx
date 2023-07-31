0183d810fba4a0690761256899d976040e07f0d0
Clément Duployez <c.duployez@gmail.com>
2023-07-31 12:45:01 +0200

    Add Header connected to backend

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,24 +1,25 @@
-import React, {useEffect, useState} from 'react';
+import React, {useEffect, useMemo, useState} from 'react';
 import './Header.scss';
 import Loader from '../Loader/Loader';
-import axios, {AxiosResponse} from 'axios';
-import {Simulate} from 'react-dom/test-utils';
-import load = Simulate.load;
 import {ProfileHttpService} from '../../helpers/http/profile-http.service';
-import {ProfileDtoMapper} from '../../mappers/dtos/profile-dto-mapper';
 import {ProfileDto} from '../../model/dtos/profile-dto';
-
-const staticObj = {i: 0};
+import {UseStateType} from '../../types/use-state-type';
+import {ConfigService} from '../../helpers/config/config-service';
 
 function Header() {
-    const profileHttpService: ProfileHttpService = new ProfileHttpService();
+    const configService: ConfigService = useMemo(() => new ConfigService(), []);
 
-    const [loading, setLoading]: [boolean, any] = useState(false);
-    const [profile, setProfile]: [ProfileDto | null, any] = useState<ProfileDto | null>(null);
-    const [leftInfoList, setLeftInfoList]: [string[], any] = useState([]);
-    const [rightInfoList, setRightInfoList]: [string[], any] = useState([]);
+    let keyIndex: number = 0;
+    const goalTitle: string = 'Projet professionnel';
+
+    const [loading, setLoading]: UseStateType<boolean> = useState<boolean>(false);
+    const [profile, setProfile]: UseStateType<ProfileDto | null> = useState<ProfileDto | null>(null);
+    const [leftInfoList, setLeftInfoList]: UseStateType<string[]> = useState<string[]>([]);
+    const [rightInfoList, setRightInfoList]: UseStateType<string[]> = useState<string[]>([]);
 
     useEffect(() => {
+        const profileHttpService: ProfileHttpService = new ProfileHttpService(configService);
+
         setLoading(true);
         profileHttpService.get().then((profile: ProfileDto) => {
             setProfile(profile);
@@ -35,7 +36,15 @@ function Header() {
             }
             setLoading(false);
         });
-    }, []);
+    }, [configService]);
+
+    function toLi(infoList: string[]): React.JSX.Element[] {
+        return infoList.map((info: string) => <li key={keyIndex++}>{info}</li>);
+    }
+
+    function toGoalElements(goals: string[]): React.JSX.Element[] {
+        return goals.map((goal: string) => <p key={keyIndex++}>{goal}</p>);
+    }
 
     function loader() {
         return <Loader/>;
@@ -48,11 +57,38 @@ function Header() {
     if (profile) {
         return (
             <section className="container">
+                <div className="side side-left">
+                    <img className={'light-theme'} src={configService.avatarUrl}/>
+                </div>
+
                 <div className="main">
                     <h1>{profile.fullName}</h1>
+                    <div className="main-grid info">
+                        <div className="main-grid-col main-grid-col-left">
+                            <ul>{toLi(leftInfoList)}</ul>
+                        </div>
+                        <div className="main-grid-col main-grid-col-right">
+                            <ul>{toLi(rightInfoList)}</ul>
+                        </div>
+                    </div>
+                    <div className="main-grid contact">
+                        <div className="main-grid-col flex-merge-row">
+                            <ul>
+                                <li>
+                                    <a href={`mailto:${profile.email}`}>{profile.email}</a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="side side-right">
+                    <h2>{goalTitle}</h2>
+                    {toGoalElements(profile.goals)}
                 </div>
             </section>
-        );
+        )
+            ;
     }
 
     return (
--- src/helpers/config/config-service.ts
+++ src/helpers/config/config-service.ts
@@ -1,13 +1,13 @@
 export class ConfigService {
-    readonly avatarUrl: string | null = process.env.REACT_APP_AVATAR_URL || null;
+    readonly avatarUrl: string | undefined = process.env.REACT_APP_AVATAR_URL || 'https://avatars1.githubusercontent.com/u/60548405?s=460&v=4';
 
-    readonly backResumeUrl: string | null = process.env.REACT_APP_BACK_RESUME_URL || 'http://localhost:10001';
+    readonly backResumeUrl: string | undefined = process.env.REACT_APP_BACK_RESUME_URL || 'http://localhost:10001';
 
-    readonly githubUrl: string | null = process.env.REACT_APP_GITHUB_URL || null;
+    readonly githubUrl: string | undefined = process.env.REACT_APP_GITHUB_URL || undefined;
 
-    readonly linkedInUrl: string | null = process.env.REACT_APP_LINKEDIN_URL || null;
+    readonly linkedInUrl: string | undefined = process.env.REACT_APP_LINKEDIN_URL || undefined;
 
-    readonly pdfDownloadUrl: string | null = process.env.REACT_APP_PDF_DOWNLOAD_URL || null;
+    readonly pdfDownloadUrl: string | undefined = process.env.REACT_APP_PDF_DOWNLOAD_URL || undefined;
 
-    readonly latestRevisionYear: string | null = process.env.REACT_APP_LATEST_REVISION_YEAR || null;
+    readonly latestRevisionYear: string | undefined = process.env.REACT_APP_LATEST_REVISION_YEAR || undefined;
 }
