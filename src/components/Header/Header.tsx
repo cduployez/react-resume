e0fb28672d0a0ef5b9ad21fc3346a76725fff1ca
Clément Duployez <c.duployez@gmail.com>
2023-08-01 15:46:35 +0200

    Fix use effect dependency

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -9,10 +9,13 @@ import Banner from '../Banner/Banner';
 
 const configService: ConfigService = new ConfigService(process.env);
 
+const messages: { goalTitle: string } = {
+    goalTitle: 'Projet professionnel'
+};
+
 function Header(): React.JSX.Element {
 
     let keyIndex: number = 0;
-    const goalTitle: string = 'Projet professionnel';
 
     const [loading, setLoading]: UseStateType<boolean> = useState<boolean>(false);
     const [profile, setProfile]: UseStateType<ProfileDto | null> = useState<ProfileDto | null>(null);
@@ -38,9 +41,9 @@ function Header(): React.JSX.Element {
             }
             setLoading(false);
         });
-    }, [configService]);
+    }, []);
 
-    function toLi(infoList: string[]): React.JSX.Element[] {
+    function toLiElements(infoList: string[]): React.JSX.Element[] {
         return infoList.map((info: string) => <li key={keyIndex++}>{info}</li>);
     }
 
@@ -48,56 +51,51 @@ function Header(): React.JSX.Element {
         return goals.map((goal: string) => <p key={keyIndex++}>{goal}</p>);
     }
 
-    function loader() {
+    if (loading) {
         return <Loader/>;
     }
 
-    if (loading) {
-        return loader();
-    }
-
-    if (profile) {
-        return (
-            <>
-                <section className={`${styles.Header} ${styles.container}`}>
-                    <div className={`${styles.side} ${styles.sideLeft}`}>
-                        <img className={'light-theme'} src={configService.avatarUrl} alt="avatar"/>
-                    </div>
-
-                    <div className={`${styles.main}`}>
-                        <h1>{profile.fullName}</h1>
-                        <div className={`${styles.mainGrid} info`}>
-                            <div className={`${styles.mainGridCol} ${styles.mainGridColLeft}`}>
-                                <ul>{toLi(leftInfoList)}</ul>
-                            </div>
-                            <div className={`${styles.mainGridCol} ${styles.mainGridColRight}`}>
-                                <ul>{toLi(rightInfoList)}</ul>
-                            </div>
-                        </div>
-                        <div className={`${styles.mainGrid} ${styles.contact}`}>
-                            <div className={`${styles.mainGridCol} ${styles.flexMergeRow}`}>
-                                <ul>
-                                    <li>
-                                        <a href={`mailto:${profile.email}`}>{profile.email}</a>
-                                    </li>
-                                </ul>
-                            </div>
-                        </div>
-                    </div>
-
-                    <div className={`${styles.side} ${styles.sideRight}`}>
-                        <h2>{goalTitle}</h2>
-                        {toGoalElements(profile.goals)}
-                    </div>
-                </section>
-                <Banner position={profile.position}/>
-            </>
-        );
+    if (!profile) {
+        return <section className={`${styles.Header} ${styles.container}`}></section>;
     }
 
     return (
-        <section className={`${styles.Header} ${styles.container}`}></section>
+        <>
+            <section className={`${styles.Header} ${styles.container}`}>
+                <div className={`${styles.side} ${styles.sideLeft}`}>
+                    <img src={configService.avatarUrl} alt="avatar"/>
+                </div>
+
+                <div className={`${styles.main}`}>
+                    <h1>{profile.fullName}</h1>
+                    <div className={`${styles.mainGrid} info`}>
+                        <div className={`${styles.mainGridCol} ${styles.mainGridColLeft}`}>
+                            <ul>{toLiElements(leftInfoList)}</ul>
+                        </div>
+                        <div className={`${styles.mainGridCol} ${styles.mainGridColRight}`}>
+                            <ul>{toLiElements(rightInfoList)}</ul>
+                        </div>
+                    </div>
+                    <div className={`${styles.mainGrid} ${styles.contact}`}>
+                        <div className={`${styles.mainGridCol} ${styles.flexMergeRow}`}>
+                            <ul>
+                                <li>
+                                    <a href={`mailto:${profile.email}`}>{profile.email}</a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+
+                <div className={`${styles.side} ${styles.sideRight}`}>
+                    <h2>{messages.goalTitle}</h2>
+                    {toGoalElements(profile.goals)}
+                </div>
+            </section>
+            <Banner position={profile.position}/>
+        </>
     );
 }
+
 
 export default Header;
