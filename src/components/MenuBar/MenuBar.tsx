336ef5f40d1d4801aaba7fea16cc9e41036f856e
cduployez <c.duployez@gmail.com>
2023-08-01 16:22:42 +0200

    Add application icons

--- src/components/MenuBar/MenuBar.tsx
+++ src/components/MenuBar/MenuBar.tsx
@@ -3,8 +3,11 @@ import {useState} from 'react';
 import {ConfigService} from '../../helpers/config/config-service';
 import classNames from 'classnames';
 import styles from './MenuBar.module.scss';
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+import {IconsService} from '../../helpers/ui/icons.service';
 
 const configService: ConfigService = new ConfigService(process.env);
+const iconsService: IconsService = new IconsService();
 
 function MenuBar(): React.JSX.Element {
     let [minimized, setMinimized]: UseStateType<boolean> = useState(sessionStorage.menuBarMinized || false);
@@ -21,6 +24,17 @@ function MenuBar(): React.JSX.Element {
     return (
         <nav className={classNames({[styles.MenuBar]: true, [styles.minimized]: minimized})}>
             {titleElement()}
+            <div>
+                <a href={configService.pdfDownloadUrl} className={`${styles.pdf} ${styles.link}`}>
+                    <FontAwesomeIcon icon={iconsService.faFilePdf}/>
+                </a>
+                <a href={configService.linkedInUrl} className={`${styles.linkedin} ${styles.link}`}>
+                    <FontAwesomeIcon icon={iconsService.faLinkedIn}/>
+                </a>
+                <a href={configService.githubUrl} className={`${styles.github} ${styles.link}`}>
+                    <FontAwesomeIcon icon={iconsService.faGithub}/>
+                </a>
+            </div>
         </nav>
     );
 
