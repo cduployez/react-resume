7b971c25f97af96482494222d728e6971f0e20b8
Clément Duployez <c.duployez@gmail.com>
2023-08-04 18:39:58 +0200

    Add active skill on click

--- src/components/Skill/Skill.tsx
+++ src/components/Skill/Skill.tsx
@@ -1,6 +1,8 @@
 import styles from './Skill.module.scss';
 import classNames from 'classnames';
 import { SkillEnum } from '../../model/enums/skill.enum';
+import { useDispatch, useSelector } from 'react-redux';
+import { ActiveSkill, addActiveSkill } from '../../redux/actions/action';
 
 interface SkillProps {
   title: string;
@@ -10,13 +12,29 @@ interface SkillProps {
 }
 
 function Skill(props: SkillProps): React.JSX.Element {
-  // const skillEnums: SkillEnum[] = props.skillEnum ? [props.skillEnum] : [];
+  const activeSkills = useSelector(
+    (state: { activeSkill: ActiveSkill }) => state.activeSkill
+  );
+  const dispatch = useDispatch();
+
+  function checkActive(): boolean {
+    return activeSkills?.parent === props.skillEnum;
+  }
 
   return (
-    <div className={styles.iconWithTitle}>
+    <div
+      className={styles.iconWithTitle}
+      onClick={() =>
+        dispatch(
+          addActiveSkill({
+            parent: props.skillEnum || null,
+            children: props.childrenKeywords
+          })
+        )
+      }>
       <div
         className={classNames({
-          [styles.active]: false,
+          [styles.active]: checkActive(),
           [styles.icon]: true
         })}>
         {props.contentElement}
---
+++ src/redux/actions/action.ts
@@ -0,0 +1,36 @@
+import { SkillEnum } from '../../model/enums/skill.enum';
+import { ActionEnum } from './action-enum';
+
+export interface ActiveSkill {
+  parent: SkillEnum | null;
+  children: SkillEnum[];
+}
+
+interface ActiveSkillProps {
+  type: ActionEnum;
+  activeSkill: ActiveSkill;
+}
+
+export interface AddActiveSkillProps extends ActiveSkillProps {
+  type: ActionEnum.ADD_ACTIVE_SKILL;
+}
+
+export interface RemoveActiveSkillProps extends ActiveSkillProps {
+  type: ActionEnum.REMOVE_ACTIVE_SKILL;
+}
+
+export function addActiveSkill(activeSkill: ActiveSkill): AddActiveSkillProps {
+  return {
+    type: ActionEnum.ADD_ACTIVE_SKILL,
+    activeSkill: activeSkill
+  };
+}
+export function removeActiveSkill(activeSkill: {
+  parent: SkillEnum;
+  children: SkillEnum[];
+}): RemoveActiveSkillProps {
+  return {
+    type: ActionEnum.REMOVE_ACTIVE_SKILL,
+    activeSkill: activeSkill
+  };
+}
